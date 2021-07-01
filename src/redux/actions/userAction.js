44ba5f427ce7955f7589c4c0574c45e547a1f4e4
akash verma <verma.akash045@gmail.com>
2021-07-01 21:18:36 +0530

    update

--- src/redux/actions/userAction.js
+++ src/redux/actions/userAction.js
@@ -4,12 +4,12 @@ import * as userActionType from "../actionsType/userActionType";
 
 export const getUser = (params) => async (dispatch) => {
     const response = await userApi.getUser(params);
-    // if (response.data.success) {
-    //     dispatch({
-    //         type: userActionType.GET_USER,
-    //         payload: response.data.data
-    //     });
-    // }
+    if (response.data.success) {
+        dispatch({
+            type: userActionType.GET_USER,
+            payload: response.data.data
+        });
+    }
     return response;
 };
 
